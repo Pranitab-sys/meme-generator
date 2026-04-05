63d2937cbf84f87d09604a2973b035ad9d5e9995
kalyani <kalyanipatil1310@gmail.com>
2026-04-05 09:01:29 +0530

    added

--- script.js
+++ script.js
@@ -32,9 +32,27 @@ function generateMeme() {
         return;
     }
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(image, 0, 0);
+    const maxWidth = 400;
+    const maxHeight = 400;
 
+    // Calculate scale
+    let ratio = Math.min(maxWidth / image.width, maxHeight / image.height);
+
+    let newWidth = image.width * ratio;
+    let newHeight = image.height * ratio;
+
+    // Set canvas fixed size
+    canvas.width = maxWidth;
+    canvas.height = maxHeight;
+
+    // Center image
+    let x = (maxWidth - newWidth) / 2;
+    let y = (maxHeight - newHeight) / 2;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(image, x, y, newWidth, newHeight);
+
+    // TEXT
     const top = document.getElementById("topText").value;
     const bottom = document.getElementById("bottomText").value;
     const size = document.getElementById("fontSize").value;
@@ -53,6 +71,7 @@ function generateMeme() {
     ctx.fillText(bottom, canvas.width / 2, canvas.height - 20);
     ctx.strokeText(bottom, canvas.width / 2, canvas.height - 20);
 }
+   
 
 function downloadMeme() {
     const link = document.createElement("a");
